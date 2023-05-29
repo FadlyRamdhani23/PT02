04c02fba37f029571f524aa24c05bb1b125a2755
Gabriel David Florens Ayal <gdfayal@gmail.com>
2023-05-29 07:48:00 +0700

    Layout Login register

--- app/src/main/java/org/project2/hairpasstudio/ui/login/LoginActivity.kt
+++
@@ -1,7 +0,0 @@
-package org.project2.hairpasstudio.ui.login
-
-import androidx.fragment.app.Fragment
-
-class LoginActivity : Fragment() {
-
-}
---
+++ app/src/main/java/org/project2/hairpasstudio/ui/login/LoginFragmennt.kt
@@ -0,0 +1,9 @@
+package org.project2.hairpasstudio.ui.login
+
+import androidx.fragment.app.Fragment
+import org.project2.hairpasstudio.databinding.FragmentLoginBinding
+
+class LoginFragmennt : Fragment() {
+    private lateinit var binding : FragmentLoginBinding
+
+}
